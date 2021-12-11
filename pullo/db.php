9e498379d19c18cc52e67f87b6a5e22f82c892f5
ChetanNain <nain.chetan10@gmail.com>
2021-12-11 06:30:13 -0800

    updated db

--- pullo/db.php
+++ pullo/db.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'cmpe');
+define('DB_SERVER', 'database-1.caorc3vfgkkg.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'Password123');
+define('DB_NAME', 'shoppingdb');
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
